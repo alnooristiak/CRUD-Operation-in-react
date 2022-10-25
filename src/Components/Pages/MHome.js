6de3e98423b7baaa091aa5a11d4533ef10663bcc
Al Noor Istiak Mahmud <istiakmahmud1010@gmail.com>
2022-10-25 16:23:07 +0600

    adding student info data section ui

--- src/Components/Pages/MHome.js
+++ src/Components/Pages/MHome.js
@@ -1,5 +1,8 @@
 import { Box, Typography } from '@mui/material';
 import React from 'react';
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+import CreateIcon from '@mui/icons-material/Create';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const MHome = () => {
     return (
@@ -14,7 +17,7 @@ const MHome = () => {
                     {/* Data form */}
                     <div className="col-lg-6 col-md-6 col-12">
                         <div className='bg-warning text-center p-2'>
-                            <h4>Input Student Data</h4>
+                            <h4>Add Student</h4>
                         </div>
                         <div>
                             <form action="" className='p-2'>
@@ -35,7 +38,47 @@ const MHome = () => {
                     {/* Data viewer */}
                     <div className="col-lg-6 col-md-6 col-12">
                         <div className="bg-info text-center p-2">
-                            <h4>Input Student Data</h4>
+                            <h4>Student Data</h4>
+                        </div>
+                        <div className=" ">
+                            {/* header text */}
+                            <div className='row'>
+                                <div className="col-2 text-center">
+                                    <span className='text-info text-warning'>No</span>
+                                </div>
+                                <div className="col-3 text-center">
+                                    <span className='text-info text-warning'>Name</span>
+                                </div>
+                                <div className="col-4 text-center">
+                                    <span className='text-info text-warning'>Email</span>
+                                </div>
+                                <div className="col-3 text-center">
+                                    <span className='text-info text-warning'>Action</span>
+                                </div>
+                            </div>
+                            {/* student data viewer info */}
+                            <div className='row bg-secondary p-1'>
+                                <div className="col-2 text-center">
+                                    <span className='text-white'>01</span>
+                                </div>
+                                <div className="col-3 text-center">
+                                    <span className='text-white'>al noor istiak</span>
+                                </div>
+                                <div className="col-4 text-center">
+                                    <span className='text-white'>istiak@gmail.com</span>
+                                </div>
+                                <div className="col-3 text-center">
+                                    <span className='text-info'>
+                                        <span><RemoveRedEyeIcon /></span>
+                                    </span>
+                                    <span className='text-info'>
+                                        <CreateIcon/>
+                                    </span>
+                                    <span className='text-danger'>
+                                        <DeleteIcon/>
+                                    </span>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
