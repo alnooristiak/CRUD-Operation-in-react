b4bebba2f35d5ed1e4c570b826b7500fb6d8a973
Al Noor Istiak Mahmud <istiakmahmud1010@gmail.com>
2022-10-25 15:57:17 +0600

    adding form section ui

--- src/Components/Pages/MHome.js
+++ src/Components/Pages/MHome.js
@@ -4,22 +4,43 @@ import React from 'react';
 const MHome = () => {
     return (
         <div>
-            <Box textTransform="center"  sx={{ bgcolor: 'secondary.main' }} className="text-center text-white py-2">
+            <Box textTransform="center" sx={{ bgcolor: 'secondary.main' }} className="text-center text-white py-2">
                 <Typography variant='h3'>This is React Croud Operation...</Typography>
             </Box>
 
             {/* Data from and viever section  */}
             <div className="container">
-                {/* Data form */}
-                <div className="col">
-                    <h4>Input Student Data</h4>
-                </div>
-                {/* Data viewer */}
-                <div className="col">
-                    <h4>Input Student Data</h4>
+                <div className="row my-3">
+                    {/* Data form */}
+                    <div className="col-lg-6 col-md-6 col-12">
+                        <div className='bg-warning text-center p-2'>
+                            <h4>Input Student Data</h4>
+                        </div>
+                        <div>
+                            <form action="" className='p-2'>
+                                <div class="mb-3">
+                                    <label for="Enter-neme" class="form-label">Enter Name</label>
+                                    <input type="text" class="form-control" id="Enter-neme" aria-describedby="emailHelp" />
+                                </div>
+                                <div class="mb-3">
+                                    <label for="Enter-email" class="form-label">Enter Email</label>
+                                    <input type="email" class="form-control" id="Enter-email" aria-describedby="emailHelp" />
+                                </div>
+                                <div className='text-center'>
+                                    <button type="submit" class="btn btn-warning text-white">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    {/* Data viewer */}
+                    <div className="col-lg-6 col-md-6 col-12">
+                        <div className="bg-info text-center p-2">
+                            <h4>Input Student Data</h4>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
